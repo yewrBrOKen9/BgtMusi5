33319a37340b18afc37773f6d4e17dff03b2b7d8
yewrBrOKen9 <126083040+yewrBrOKen9@users.noreply.github.com>
2023-04-05 07:09:41 +0530

    Update stop.py

--- plugins/admins/stop.py
+++ plugins/admins/stop.py
@@ -23,7 +23,7 @@ from modules.utils.decorators import AdminRightsCheck
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
-        return await message.reply_text("**❌ ᴇʀʀᴏʀ, ᴡʀᴏʙɢ ᴜsᴀsᴀᴇᴇ ᴏғ ᴄᴏᴍᴍᴀɴᴅ❗...**")
+        return await message.reply_text("**❌ ᴇʀʀᴏʀ, ᴡʀᴏʙɢ ᴜsᴇs ᴏғ ᴄᴏᴍᴍᴀɴᴅ❗...**")
     await Aditya.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
